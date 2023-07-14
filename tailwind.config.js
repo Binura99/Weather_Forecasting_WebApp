a6b0cef0dfd6a89bb1aa9679d08e7cf75535176c
Binura99 <binurayasith12@gmail.com>
2023-07-14 22:55:33 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+
+    fontFamily: {
+          
+      lexendG : ['Lexend Giga'],
+      AlegreyaSansSC : ['Alegreya Sans SC'],
+      Lobster : ['Lobster'],
+      LexandExa : ['Lexend Exa'],
+      
+    },
+  },
+  plugins: [],
+}
